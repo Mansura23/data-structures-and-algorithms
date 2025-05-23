0c3a9e9e61ad85777fd2ce2518313658db2b2b9f
Mansura <badalovamansura40@gmail.com>
2025-05-23 19:40:29 +0400

    added succesfully

---
+++ src/main/java/LeetCode/PlusOne.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            else {
+                digits[i] = 0;
+            }
+
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+    public static void main(String[] args) {
+        int[] digits ={1,2,3};
+        PlusOne plusOne = new PlusOne();
+        System.out.println(Arrays.toString(plusOne.plusOne(digits)));
+    }
+}
